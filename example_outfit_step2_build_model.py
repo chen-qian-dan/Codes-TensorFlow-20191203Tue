216c432dc5a34d60cf3070125b273ca654d8e628
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-03 18:58:39 +1030

    example_outfit_step2_build_model.

---
+++ example_outfit_step2_build_model.py
@@ -0,0 +1,45 @@
+
+# import libraries
+import tensorflow as tf
+from tensorflow import keras # API of tensorflow
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load data------------------------------------------
+data = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot"
+]
+
+# scale images
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# build model----------------------------------------------
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Tested ACC: ", test_acc)
+
+
+
